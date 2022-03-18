d14dfb64c23bc7a4f3bf5265167c50e90e4180d9
liulinjie <liulinjie@zhihu.com>
2022-03-18 17:38:19 +0800

    second test

--- app/src/main/java/com/llj/iotlibruary/MainActivity.kt
+++ app/src/main/java/com/llj/iotlibruary/MainActivity.kt
@@ -2,11 +2,11 @@ package com.llj.iotlibruary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.llj.baselib.TestLib
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
--- baselib/src/main/java/com/llj/baselib/TestLib.kt
+++ baselib/src/main/java/com/llj/baselib/TestLib.kt
@@ -1,4 +1,5 @@
 package com.llj.baselib
 
 class TestLib {
+    fun test() = "suc"
 }
